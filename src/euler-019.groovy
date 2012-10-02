a3a1b37133a730447f0a5a1684fb3705a479d7bc
mathieucarbou <mathieu.carbou@gmail.com>
2012-10-02 08:06:35 -0400

    euler 19

---
+++ src/euler-019.groovy
@@ -0,0 +1,10 @@
+import org.joda.time.LocalDate
+
+def max = new LocalDate(2000, 12, 31), count = 0
+for(LocalDate day = new LocalDate(1901, 1, 1); day.isBefore(max); day = day.plusMonths(1)) {
+    if(day.dayOfWeek == 7) {
+        println "${day}"
+        count++
+    }
+}
+println count
