291608fa5a7435dbabb4223663ba5192d5405e79
mathieucarbou <mathieu.carbou@gmail.com>
2012-11-05 00:11:46 -0500

    euler 14

--- src/euler-014.groovy
+++ src/euler-014.groovy
@@ -1,4 +1,24 @@
-def collatz, nbTerms = [1L: 1]
-collatz = {nbTerms[it] ?: (nbTerms[it] = 1 + collatz(it & 0x1 ? (it + (it << 1) + 1L) : (it >> 1)))}
-for (N in 999999L..2L) collatz(N)
+/*
+    http://oeis.org/A006877
+    => we start from n=837799
+    => we skip all multiples of 2 because n/2 would no produce a longer sequence than 837799
+*/
+Map<Long, Integer> nbTerms = [(1L): 1]
+for (long n = 837799L; n <= 999999L; n += 2) {
+    int nb = 1
+    long i = 3 * n + 1
+    while (!nbTerms[i]) {
+        while ((i & 1) == 0) {
+            i = i >> 1
+            nb++
+        }
+        if (nbTerms[i]) {
+            nb += nbTerms[i]
+        } else {
+            i = 3 * i + 1
+            nb++
+        }
+    }
+    nbTerms.put(n, nb)
+}
 println(nbTerms.max {it.value}.key)
