c1a27cbc0371773c0b7dbe46a2d3fb2fd77a9051
mathieucarbou <mathieu.carbou@gmail.com>
2013-03-18 13:24:23 -0400

    project euler

--- src/all.groovy
+++ src/all.groovy
@@ -1,4 +1,4 @@
-def gp = System.getProperty('os.name').toLowerCase().contains('windows') ? 'gp-2.6.exe' : 'gp'
+def gp = System.getProperty('os.name').toLowerCase().contains('windows') ? 'C:\\cygwin\\usr\\local\\bin\\gp-2.6.exe' : 'gp'
 long wtime = System.currentTimeMillis()
 new File(".").eachFile {
     if (!(it.name in ['all.groovy', 'euler.groovy', 'euler.gp', 'map.gp'])) {
@@ -7,15 +7,19 @@ new File(".").eachFile {
             long time = System.currentTimeMillis()
             run(it)
             time = System.currentTimeMillis() - time
-            if(time >= 1000) println("  |-- ${time.toString().padLeft(6, '_')} ms")
+            if (time >= 1000) println("  |-- ${time.toString().padLeft(6, '_')} ms")
         } else if (it.name.endsWith('.gp')) {
             print("[GP] ${it.name[0..-4]} ==> ")
             long time = System.currentTimeMillis()
-            def proc = [gp, '-q', '-s', '30M', '-p', '2M', it.name].execute()
-            proc.waitFor()
+            try {
+                def proc = [gp, '-q', '-s', '30M', '-p', '2M', it.name].execute()
+                proc.waitFor()
+                print(proc.inputStream.text ?: proc.errorStream.text)
+            } catch (e) {
+                println e.message
+            }
             time = System.currentTimeMillis() - time
-            print (proc.inputStream.text ?: proc.errorStream.text)
-            if(time >= 1000) println("  |-- ${time.toString().padLeft(6, '_')} ms")
+            if (time >= 1000) println("  |-- ${time.toString().padLeft(6, '_')} ms")
         }
     }
 }
